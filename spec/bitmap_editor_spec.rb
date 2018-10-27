9c860dc493bddc1b128c617748f6eb61abf7f53e
Josh Bebbington <josh.bebbington92@gmail.com>
2018-10-27 23:47:43 +0100

    Organised validation specs into a context per command

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -15,53 +15,65 @@ describe BitmapEditor do
     it { expect { subject }.to raise_error('Command not recognised') }
   end
 
-  context 'receives I command with too few arguments' do
-    let(:content) { 'I 2' }
-    it { expect { subject }.to raise_error('Invalid number of arguments') }
+  context 'validates S command' do 
+    context 'too many arguments' do
+      let(:content) { 'S 1' }
+      it { expect { subject }.to raise_error('Invalid number of arguments') }
+    end
   end
 
-  context 'receives I command with too many arguments' do
-    let(:content) { 'I 1 2 4 5 7' }
-    it { expect { subject }.to raise_error('Invalid number of arguments') }
+  context 'validates C command' do
+    context 'too many arguments' do
+      let(:content) { 'C 1' }
+      it { expect { subject }.to raise_error('Invalid number of arguments') }
+    end
   end
 
-  context 'receives S command with too many arguments' do 
-    let(:content) { 'S 1' }
-    it { expect { subject }.to raise_error('Invalid number of arguments') }
+  context 'validates I command' do
+    context 'too few arguments' do
+      let(:content) { 'I 2' }
+      it { expect { subject }.to raise_error('Invalid number of arguments') }
+    end
+
+    context 'too many arguments' do
+      let(:content) { 'I 1 2 4 5 7' }
+      it { expect { subject }.to raise_error('Invalid number of arguments') }
+    end
   end
 
-  context 'receives C command with too many arguments' do 
-    let(:content) { 'C 1' }
-    it { expect { subject }.to raise_error('Invalid number of arguments') }
+  context 'validates L command' do
+    context 'too few arguments' do
+      let(:content) { 'L X Y' }
+      it { expect { subject }.to raise_error('Invalid number of arguments') }
+    end
+
+    context 'too many arguments' do
+      let(:content) { 'L X Y C A' }
+      it { expect { subject }.to raise_error('Invalid number of arguments') }
+    end
   end
 
-  context 'receives L command with too few arguments' do 
-    let(:content) { 'L X Y' }
-    it { expect { subject }.to raise_error('Invalid number of arguments') }
+  context 'validates V command' do
+    context 'too few arguments' do
+      let(:content) { 'V X' }
+      it { expect { subject }.to raise_error('Invalid number of arguments') }
+    end
+
+    context 'too many arguments' do
+      let(:content) { 'V X Y1 Y2 C A' }
+      it { expect { subject }.to raise_error('Invalid number of arguments') }
+    end
   end
 
-  context 'receives L command with too few arguments' do 
-    let(:content) { 'L X Y C A' }
-    it { expect { subject }.to raise_error('Invalid number of arguments') }
-  end
-  
-  context 'receives V command with too few arguments' do 
-    let(:content) { 'V X Y1' }
-    it { expect { subject }.to raise_error('Invalid number of arguments') }
-  end
+  context 'validates H command' do
+    context 'too few arguments' do
+      let(:content) { 'H X1 X2' }
+      it { expect { subject }.to raise_error('Invalid number of arguments') }
+    end
 
-  context 'receives V command with too many arguments' do 
-    let(:content) { 'V X Y1 Y2 C A' }
-    it { expect { subject }.to raise_error('Invalid number of arguments') }
-  end
-
-  context 'receives H command with too few arguments' do 
-    let(:content) { 'H X1 X2' }
-    it { expect { subject }.to raise_error('Invalid number of arguments') }
-  end
-  
-  context 'receives H command with too many arguments' do 
-    let(:content) { 'H X1 X2 C A' }
-    it { expect { subject }.to raise_error('Invalid number of arguments') }
+    context 'too many arguments' do
+      let(:content) { 'H X1 X2 C A' }
+      it { expect { subject }.to raise_error('Invalid number of arguments') }
+    end
   end
 end
