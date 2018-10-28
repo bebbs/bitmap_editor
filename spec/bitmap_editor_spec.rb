6b33d7dc3304d22c270e1cb5a8cfcb5202e06856
Josh Bebbington <josh.bebbington92@gmail.com>
2018-10-28 01:16:23 +0100

    Added higher level tests for behaviour of BitmapEditor#run

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -2,11 +2,23 @@ require 'bitmap_editor'
 require 'tempfile'
 
 describe BitmapEditor do
-  let(:subject) { described_class.new.run path }
-  let(:path) do
-    Tempfile.new.tap do |file|
-      file.write(content)
-      file.rewind
+  context '#run' do
+    let(:subject) { described_class.new.run path }
+    let(:path) do
+      Tempfile.new.tap do |file|
+        file.write(content)
+        file.rewind
+      end
+    end
+
+    context 'with a valid file' do
+      let(:content) { 'I 2 3' }
+      it { expect { subject }.not_to raise_error }
+    end
+
+    context 'with an invalid file' do
+      let(:content) { 'bad_content' }
+      it { expect { subject }.to raise_error 'Command not recognised' }
     end
   end
 end
