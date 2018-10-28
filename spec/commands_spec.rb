312213a6cd700ace3b44ed25abf588ea5b985954
Josh Bebbington <josh.bebbington92@gmail.com>
2018-10-28 22:10:28 +0000

    Refactored command specs to use a dynamic context per command class

--- spec/commands_spec.rb
+++ spec/commands_spec.rb
@@ -9,138 +9,138 @@ describe 'Commands' do
     :show => true)
   }
 
-  context 'validates show' do
-    it 'too many arguments' do
-      cmd = Commands::Show.new('S 1', nil)
+  context Commands::Show do
+    it 'rejects too many arguments' do
+      cmd = described_class.new('S 1', nil)
       expect { cmd.call }.to raise_error(Commands::InvalidCommandError, 'Invalid number of arguments')
     end
 
-    it 'when canvas does not exist' do
-      cmd = Commands::Show.new('S', nil)
+    it 'checks that canvas exists' do
+      cmd = described_class.new('S', nil)
       expect { cmd.call }.to raise_error(Commands::NilCanvasError, 'Command must be called after creating a canvas')
     end
 
-    it 'send show command to canvas' do
-      cmd = Commands::Show.new('S', canvas)
+    it 'sends show command to canvas' do
+      cmd = described_class.new('S', canvas)
       cmd.call
       expect(canvas).to have_received(:show)
     end
   end
 
-  context 'validates clear' do
-    it 'too many arguments' do
-      cmd = Commands::Clear.new('C 1', nil)
+  context Commands::Clear do
+    it 'rejects too many arguments' do
+      cmd = described_class.new('C 1', nil)
       expect { cmd.call }.to raise_error(Commands::InvalidCommandError, 'Invalid number of arguments')
     end
 
-    it 'when canvas does not exist' do
-      cmd = Commands::Clear.new('C', nil)
+    it 'checks that canvas exists' do
+      cmd = described_class.new('C', nil)
       expect { cmd.call }.to raise_error(Commands::NilCanvasError, 'Command must be called after creating a canvas')
     end
 
     it 'sends clear command to canvas' do
-      cmd = Commands::Clear.new('C', canvas)
+      cmd = described_class.new('C', canvas)
       cmd.call
       expect(canvas).to have_received(:clear)
     end
   end
 
-  context 'validates create' do
-    it 'too few arguments' do
-      cmd = Commands::Create.new('I 2', nil)
+  context Commands::Create do
+    it 'rejects too few arguments' do
+      cmd = described_class.new('I 2', nil)
       expect { cmd.call }.to raise_error(Commands::InvalidCommandError, 'Invalid number of arguments')
     end
 
-    it 'too many arguments' do
-      cmd = Commands::Create.new('I 1 2 4 5 7', nil)
+    it 'rejects too many arguments' do
+      cmd = described_class.new('I 1 2 4 5 7', nil)
       expect { cmd.call }.to raise_error(Commands::InvalidCommandError, 'Invalid number of arguments')
     end
 
-    it 'coordinates must be integers' do
-      cmd = Commands::Create.new('I A B', nil)
+    it 'checks that coordinates are integers' do
+      cmd = described_class.new('I A B', nil)
       expect { cmd.call }.to raise_error(Commands::InvalidCommandError, 'Coordinates must be integers')
     end
   end
 
-  context 'validates fill' do
-    it 'too few arguments' do
-      cmd = Commands::Fill.new('L 1 2', nil)
+  context Commands::Fill do
+    it 'rejects too few arguments' do
+      cmd = described_class.new('L 1 2', nil)
       expect { cmd.call }.to raise_error(Commands::InvalidCommandError, 'Invalid number of arguments')
     end
 
-    it 'too many arguments' do
-      cmd = Commands::Fill.new('L 1 2 C A', nil)
+    it 'rejects too many arguments' do
+      cmd = described_class.new('L 1 2 C A', nil)
       expect { cmd.call }.to raise_error(Commands::InvalidCommandError, 'Invalid number of arguments')
     end
 
-    it 'coordinates must be integers' do
-      cmd = Commands::Fill.new('L A B M', nil)
+    it 'checks that coordinates are integers' do
+      cmd = described_class.new('L A B M', nil)
       expect { cmd.call }.to raise_error(Commands::InvalidCommandError, 'Coordinates must be integers')
     end
 
-    it 'when canvas does not exist' do
-      cmd = Commands::Fill.new('L 1 2 M', nil)
+    it 'checks that canvas exists' do
+      cmd = described_class.new('L 1 2 M', nil)
       expect { cmd.call }.to raise_error(Commands::NilCanvasError, 'Command must be called after creating a canvas')
     end
 
-    it 'sends fill_colour command to canvas' do
-      cmd = Commands::Fill.new('L 1 2 M', canvas)
+    it 'sends fill command to canvas' do
+      cmd = described_class.new('L 1 2 M', canvas)
       cmd.call
       expect(canvas).to have_received(:fill_colour)
     end
   end
 
-  context 'validates vertical' do
-    it 'too few arguments' do
-      cmd = Commands::Vertical.new('V 1', nil)
+  context Commands::Vertical do
+    it 'rejects too few arguments' do
+      cmd = described_class.new('V 1', nil)
       expect { cmd.call }.to raise_error(Commands::InvalidCommandError, 'Invalid number of arguments')
     end
 
-    it 'too many arguments' do
-      cmd = Commands::Vertical.new('V 1 1 2 C A', nil)
+    it 'rejects too many arguments' do
+      cmd = described_class.new('V 1 1 2 C A', nil)
       expect { cmd.call }.to raise_error(Commands::InvalidCommandError, 'Invalid number of arguments')
     end
 
-    it 'coordinates must be integers' do
-      cmd = Commands::Vertical.new('V X Y Z C', nil)
+    it 'checks that coordinates are integers' do
+      cmd = described_class.new('V X Y Z C', nil)
       expect { cmd.call }.to raise_error(Commands::InvalidCommandError, 'Coordinates must be integers')
     end
 
-    it 'when canvas does not exist' do
-      cmd = Commands::Vertical.new('V 1 2 3 C', nil)
+    it 'checks that canvas exists' do
+      cmd = described_class.new('V 1 2 3 C', nil)
       expect { cmd.call }.to raise_error(Commands::NilCanvasError, 'Command must be called after creating a canvas')
     end
 
     it 'sends vertical command to canvas' do
-      cmd = Commands::Vertical.new('V 1 2 3 C', canvas)
+      cmd = described_class.new('V 1 2 3 C', canvas)
       cmd.call
       expect(canvas).to have_received(:fill_vertical)
     end
   end
 
-  context 'validates horizontal' do
-    it 'too few arguments' do
-      cmd = Commands::Vertical.new('H 1 2', nil)
+  context Commands::Horizontal do
+    it 'rejects too few arguments' do
+      cmd = described_class.new('H 1 2', nil)
       expect { cmd.call }.to raise_error(Commands::InvalidCommandError, 'Invalid number of arguments')
     end
 
-    it 'too many arguments' do
-      cmd = Commands::Vertical.new('H 1 2 3 4 C A', nil)
+    it 'rejects too many arguments' do
+      cmd = described_class.new('H 1 2 3 4 C A', nil)
       expect { cmd.call }.to raise_error(Commands::InvalidCommandError, 'Invalid number of arguments')
     end
 
-    it 'coordinates must be integers' do
-      cmd = Commands::Horizontal.new('H X Y Z C', nil)
+    it 'checks that coordinates are integers' do
+      cmd = described_class.new('H X Y Z C', nil)
       expect { cmd.call }.to raise_error(Commands::InvalidCommandError, 'Coordinates must be integers')
     end
 
-    it 'when canvas does not exist' do
-      cmd = Commands::Horizontal.new('H 1 2 3 C', nil)
+    it 'checks that canvas exists' do
+      cmd = described_class.new('H 1 2 3 C', nil)
       expect { cmd.call }.to raise_error(Commands::NilCanvasError, 'Command must be called after creating a canvas')
     end
 
     it 'sends horizontal command to canvas' do
-      cmd = Commands::Horizontal.new('H 1 2 3 C', canvas)
+      cmd = described_class.new('H 1 2 3 C', canvas)
       cmd.call
       expect(canvas).to have_received(:fill_horizontal)
     end
